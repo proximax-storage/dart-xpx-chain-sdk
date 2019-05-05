431a77e07c18146ce2be67bfb63e8971e1f3dd1a
slackve <eleazartovar123@gmail.com>
2019-05-05 19:51:08 -0400

    new example namespace

---
+++ examples/example_namespace.dart
@@ -0,0 +1,28 @@
+import 'package:xpx_catapult_sdk/api.dart';
+
+const baseUrl  = "http://54.169.118.203:3000";
+
+const networkType = MijinTest;
+
+/// Simple Namespace API request
+void main() async {
+  var config = new  NewConfig(baseUrl, networkType);
+
+  /// Creating a client instance
+  /// xpx_catapult_sdk uses the Dart's native HttpClient.
+  /// Depending on the platform, you may want to use either
+  /// the one which comes from dart:io or the BrowserClient
+  /// example:
+  /// 1- import 'package:http/browser_client.dart';
+  /// 2- var client = NewClient(config, new BrowserClient());
+  var client =  NewClient(config, null);
+
+  /// Generate Id from namespaceName
+  var ns = NewNamespaceIdFromName("prx");
+  try {
+    var result = await client.Namespace().GetNamespace(ns);
+    print(result);
+  } catch (e) {
+    print("Exception when calling Namespace->GetNamespace: $e\n");
+  }
+}
