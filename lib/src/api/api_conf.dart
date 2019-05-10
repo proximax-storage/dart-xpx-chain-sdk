657ec7eb5487762d9a81ed5ff5462b4884b9bdc3
slackve <eleazartovar123@gmail.com>
2019-05-10 15:35:19 -0400

    add ConfigNetworkType var

--- lib/src/api/api_conf.dart
+++ lib/src/api/api_conf.dart
@@ -1,9 +1,13 @@
 part of xpx_catapult_sdk ;
 
+int ConfigNetworkType = 0;
+
 // NewConfig is Config constructor according to 'baseURL' & 'networkType'
 class NewConfig {
   int networkType;
   String baseUrl;
 
-  NewConfig(this.baseUrl, this.networkType);
+  NewConfig(this.baseUrl, this.networkType){
+    ConfigNetworkType = this.networkType;
+  }
 }
