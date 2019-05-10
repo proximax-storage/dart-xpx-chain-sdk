7b0892f10160b877a76c22bc11c6b24faca4f4d5
slackve <eleazartovar123@gmail.com>
2019-05-10 09:52:48 -0400

    new NewMosaicIdFromNonceAndOwner method

--- lib/src/model/mosaic_model.dart
+++ lib/src/model/mosaic_model.dart
@@ -1,11 +1,11 @@
-part of xpx_catapult_sdk ;
+part of xpx_catapult_sdk;
 
 class Mosaic {
   BigInt id = null;
 
   BigInt amount = null;
 
-  Mosaic();
+  Mosaic([this.id, this.amount]);
 
   @override
   String toString() {
@@ -23,4 +23,50 @@ class Mosaic {
     id = v.id.toBigInt();
     amount = v.amount.toBigInt();
   }
+}
+
+BigInt NewMosaicIdFromNonceAndOwner(int nonce, String ownerPublicKey) {
+  if (ownerPublicKey.length != 64) {
+    throw ErrInvalidOwnerPublicKey;
+  }
+
+  return _generateMosaicId(nonce, ownerPublicKey);
+}
+
+Mosaic NewMosaic(BigInt mosaicId, amount) {
+  if (mosaicId == null) {
+    throw ErrNilMosaicId;
+  }
+
+  if (amount == null) {
+    throw ErrNilMosaicAmount;
+  }
+
+  if (EqualsBigInts(amount, BigInt.zero)) {
+    throw ErrNilMosaicAmount;
+  }
+
+  return new Mosaic(mosaicId, amount);
+}
+
+BigInt _generateMosaicId(int nonce, String ownerPublicKey) {
+  var nonceB = new Buffer.LittleEndian(4);
+  nonceB.writeInt32(nonce);
+
+  var result = sha3.New256();
+
+  result.update(nonceB.out, 0, nonceB.out.length);
+
+  var ownerBytes = HEX.decode(ownerPublicKey);
+
+  var t = result.process(ownerBytes);
+
+  List<dynamic> raw() {
+    return [
+      EndianLittleUint32(t.getRange(0, 4).toList()),
+      EndianLittleUint32(t.getRange(4, 8).toList()) & 0x7FFFFFFF
+    ];
+  }
+
+  return new UInt64DTO.fromJson(raw()).toBigInt();
 }
