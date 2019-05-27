f59ea4b9d785114a7e294749b88488330889439c
slackve <eleazartovar123@gmail.com>
2019-05-26 23:01:02 -0500

    add registerNamespaceTransaction Schema

--- lib/imp/buffer/buffer.dart
+++ lib/imp/buffer/buffer.dart
@@ -2,8 +2,25 @@ library xpx_catapult_sdk.buffer;
 
 import 'dart:typed_data';
 
+import 'package:flat_buffers/flat_buffers.dart' as fb;
+
 import 'package:xpx_catapult_sdk/xpx_sdk.dart';
 
-part 'buffer_schema.dart';
-part 'buffer_schema_transfer.dart';
-part 'buffer_schema_register_namespace.dart';
+part 'package:xpx_catapult_sdk/imp/buffer/schema/buffer_schema.dart';
+part 'package:xpx_catapult_sdk/imp/buffer/schema/buffer_schema_transfer.dart';
+part 'package:xpx_catapult_sdk/imp/buffer/schema/buffer_schema_register_namespace.dart';
+part 'package:xpx_catapult_sdk/imp/buffer/schema/buffer_schema_mosaic_definition.dart';
+
+part 'transactions/aggregate_transaction_buffer.dart';
+part 'transactions/alias_transaction_buffer.dart';
+part 'transactions/lock_funds_transaction_buffer.dart';
+part 'transactions/modify_contract_transaction_buffer.dart';
+part 'transactions/modify_metadata_transaction_buffer.dart';
+part 'transactions/modify_multisig_account_transaction_buffer.dart';
+part 'transactions/mosaic_alias_transaction_buffer.dart';
+part 'transactions/mosaic_definition_transaction_buffer.dart';
+part 'transactions/mosaic_supply_change_transaction_buffer.dart';
+part 'transactions/register_namespace_transaction_buffer.dart';
+part 'transactions/secret_lock_transaction_buffer.dart';
+part 'transactions/secret_proof_transaction_buffer.dart';
+part 'transactions/transfer_transaction_buffer.dart';
