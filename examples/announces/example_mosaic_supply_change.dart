f818425636a0db868cd6c1d4701272ddef51e129
slackve <eleazartovar123@gmail.com>
2019-05-30 22:30:08 -0500

    add MosaicSupplyChangeTransaction example

---
+++ examples/announces/example_mosaic_supply_change.dart
@@ -0,0 +1,34 @@
+import 'package:xpx_catapult_sdk/xpx_sdk.dart';
+
+const baseUrl = "http://bctestnet1.xpxsirius.io:3000";
+
+const networkType = PublicTest;
+
+/// Simple Account API AnnounceTransaction
+void main() async {
+  var config = new NewConfig(baseUrl, networkType);
+
+  var client = NewClient(config, null);
+
+  /// Create an Account from a given Private key.
+  var account = new Account.fromPrivateKey(
+      "C7724760A0C4E13723FF2EF491A51534D5EC45AE9A248E52DE52E91E2566ED16",
+      networkType);
+
+  /// Create a Mosaic Supply Change transaction.
+  var mosaicDefinition = new MosaicSupplyChangeTransaction(
+      // The maximum amount of time to include the transaction in the blockchain.
+      new Deadline(hours: 1),
+      MosaicSupplyType.Increase,
+      BigInt.from(7625074121122579335),
+      new BigInt.from(100000000000),
+      // The network type
+      networkType);
+
+  var stx = account.sign(mosaicDefinition);
+
+  var restTx = await client.Transaction.AnnounceTransaction(stx);
+  print(restTx);
+  print("Hash: ${stx.hash}");
+  print("Signer: ${account.publicAccount.publicKey}");
+}
