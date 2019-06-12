24540739336acd5c52af9d968aafdf4bfc6fea8c
slackve <eleazartovar123@gmail.com>
2019-06-12 14:32:48 -0500

    update example

--- examples/example_account.dart
+++ examples/example_account.dart
@@ -18,16 +18,34 @@ void main() async {
   var client = NewClient(config, null);
 
   /// Create an Address from a given public key.
-  var address = new Address.fromPublicKey(
+  var addressOne = new Address.fromPublicKey(
       "95DE2FFDCC397BB9688DA28A18A70FDD23F4CE2EF4240A4A7B6BAF5DFA07E5DC",
+      networkType);
+
+  var addressTwo = new Address.fromPublicKey(
+      "C64FA80DB046F488CC1C480454834D4CAE8284DDC14D6E93332AD02E345FF2C5",
       networkType);
 
   try {
     /// Get AccountInfo for an account.
     /// Param address - A Address object.
-    var result = await client.Account.PartialTransactions("95DE2FFDCC397BB9688DA28A18A70FDD23F4CE2EF4240A4A7B6BAF5DFA07E5DC");
+    var result = await client.Account.GetAccountInfo(addressOne);
     print(result);
   } catch (e) {
     print("Exception when calling Account->GetAccountInfo: $e\n");
   }
+
+  var adds = new Addresses();
+  adds.addresses.add(addressOne.address);
+  adds.addresses.add(addressTwo.address);
+
+  try {
+    /// Get accounts information.
+    /// Param address - A Address object.
+    var result = await client.Account.GetAccountsInfo(adds);
+    print(result);
+  } catch (e) {
+    print("Exception when calling Account->GetAccountInfo: $e\n");
+  }
+
 }
