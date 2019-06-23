1296410c969fa28c4c365cfc6d5fcca5ea5d82ad
slackve <eleazartovar123@gmail.com>
2019-06-22 20:08:45 -0500

    update examples

--- example/example_transfer.dart
+++ example/example_transfer.dart
@@ -1,41 +1,40 @@
 import 'package:xpx_chain_sdk/xpx_sdk.dart';
 
-const baseUrl = "http://bctestnet1.xpxsirius.io:3000";
-
-const networkType = publicTest;
-
 /// Simple Account API AnnounceTransaction
 void main() async {
-  var config =  NewConfig(baseUrl, networkType);
+  const baseUrl = 'http://bctestnet1.xpxsirius.io:3000';
 
-  var client = NewClient(config, null);
+  const networkType = publicTest;
+
+  final config = NewConfig(baseUrl, networkType);
+
+  final client = newClient(config, null);
 
   /// Create an Account from a given Private key.
-  var account =  Account.fromPrivateKey(
-      "1ACE45EAD3C2F0811D9F4355F35BF78483324975083BE4E503EA49DFFEA691A0",
+  final account = Account.fromPrivateKey(
+      '68f50e10e5b8be2b7e9ddb687a667d6e94dd55fe02b4aed8195f51f9a242558b',
       networkType);
 
   /// Create an Address from a given Public key.
-  var recipient =  Address.fromPublicKey(
-      "68f50e10e5b8be2b7e9ddb687a667d6e94dd55fe02b4aed8195f51f9a242558b",
+  final recipient = Address.fromPublicKey(
+      '68f50e10e5b8be2b7e9ddb687a667d6e94dd55fe02b4aed8195f51f9a242558b',
       networkType);
 
   /// Create a  transaction type transfer
-  var tx =  TransferTransaction(
+  final tx = TransferTransaction(
       // The maximum amount of time to include the transaction in the blockchain.
-       Deadline(hours: 1),
+      Deadline(hours: 1),
       // The Address of the recipient account.
       recipient,
       // The List of mosaic to be sent.
-      [Xpx(15)],
+      [xpx(1)],
       // The transaction message of 1024 characters.
-       Message.PlainMessage("From ProximaX Dart SDK"),
+      Message.plainMessage('From ProximaX Dart SDK'),
       networkType);
 
-  var stx = account.sign(tx);
+  final stx = account.sign(tx);
 
-  var restTx = await client.Transaction.AnnounceTransaction(stx);
+  final restTx = await client.transaction.announceTransaction(stx);
   print(restTx);
-  print("Hash: ${stx.hash}");
-  print("Signer: ${account.publicAccount.publicKey}");
+  print('Hash: ${stx.hash}');
 }
