f65ad197c76cd5915acedda69583c14ea140ce77
slackve <eleazartovar123@gmail.com>
2019-11-20 21:51:02 -0500

    refactor NetworkRoutesApi

--- lib/src/api/api_network_routes.dart
+++ lib/src/api/api_network_routes.dart
@@ -12,32 +12,10 @@ class NetworkRoutesApi {
   Future<NetworkType> getNetworkType() async {
     Object postBody;
 
-    // verify required params are set
-
     // create path and map variables
     final String path = '/network'.replaceAll('{format}', 'json');
 
-    // query params
-    final List<QueryParam> queryParams = [];
-    final Map<String, String> headerParams = {};
-    final Map<String, String> formParams = {};
-
-    final List<String> contentTypes = [];
-
-    final String contentType =
-        contentTypes.isNotEmpty ? contentTypes[0] : 'application/json';
-
-    if (contentType.startsWith('multipart/form-data')) {
-      const bool hasFields = false;
-      final http.MultipartRequest mp = http.MultipartRequest(null, null);
-
-      if (hasFields) {
-        postBody = mp;
-      }
-    } else {}
-
-    final response = await apiClient.invokeAPI(path, 'GET', queryParams,
-        postBody, headerParams, formParams, contentType);
+    final response = await apiClient.get(path, postBody);
 
     if (response.statusCode >= 400) {
       throw ApiException(response.statusCode, response.body);
