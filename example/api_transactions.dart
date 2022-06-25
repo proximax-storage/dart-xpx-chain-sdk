f99d6bb0dd3ae9e2cc0f754ba4fd18036697cda9
RustLangLatam <rust.lang.latam@gmail.com>
2022-06-24 21:22:15 -0500

    format code

--- example/api_transactions.dart
+++ example/api_transactions.dart
@@ -35,7 +35,7 @@ void main() async {
 
   final txnQueryParams = TransactionQueryParams()
     ..embedded = true
-    ..type = [TransactionType.transfer, TransactionType.aggregateCompleted]
+    ..type = [TransactionType.transfer]
     ..signerPublicKey = "785B6EDD55934391C4C0CBFCF5ECDCBC5104CB781178616A99167480418282BD";
 
   // Returns a [Transaction] information given a transactionId or hash.
@@ -47,46 +47,46 @@ void main() async {
     print('Exception when calling Transaction->GetTransaction: $e\n');
   }
 
-  // //Returns a List of [Transaction] information for a given List of transactionIds.
-  // try {
-  //   /// Get a transaction information given a transactionId or hash.
-  //   final result = await client.transaction
-  //       .getTransactions([hashOne, hashTwo], TransactionGroupType.confirmed);
-  //   print(result);
-  // } on Exception catch (e) {
-  //   print('Exception when calling Transaction->GetTransactions: $e\n');
-  // }
-  //
-  // // Returns the transaction status for a given hash.
-  // try {
-  //   final result = await client.transaction.getTransactionStatus(hashOne);
-  //   print(result);
-  // } on Exception catch (e) {
-  //   print('Exception when calling Transaction->GetTransactionStatus: $e\n');
-  // }
-  //
-  // // Returns an List of transaction statuses for a given List of transaction hashes.
-  // try {
-  //   final result =
-  //       await client.transaction.getTransactionsStatuses([hashOne, hashTwo]);
-  //   print(result);
-  // } on Exception catch (e) {
-  //   print('Exception when calling Transaction->GetTransactionsStatuses: $e\n');
-  // }
-  //
-  // try {
-  //   final result = await client.transaction.getTransactionEffectiveFee(hashOne);
-  //   print(result);
-  // } on Exception catch (e) {
-  //   print(
-  //       'Exception when calling Transaction->GetTransactionEffectiveFee: $e\n');
-  // }
-  //
-  // try {
-  //   final result = await client.transaction
-  //       .getTransactionsCount([TransactionType.transfer]);
-  //   print(result);
-  // } on Exception catch (e) {
-  //   print('Exception when calling Transaction->GetTransactionsCount: $e\n');
-  // }
+  //Returns a List of [Transaction] information for a given List of transactionIds.
+  try {
+    /// Get a transaction information given a transactionId or hash.
+    final result = await client.transaction
+        .getTransactions([hashOne, hashTwo], TransactionGroupType.confirmed);
+    print(result);
+  } on Exception catch (e) {
+    print('Exception when calling Transaction->GetTransactions: $e\n');
+  }
+
+  // Returns the transaction status for a given hash.
+  try {
+    final result = await client.transaction.getTransactionStatus(hashOne);
+    print(result);
+  } on Exception catch (e) {
+    print('Exception when calling Transaction->GetTransactionStatus: $e\n');
+  }
+
+  // Returns an List of transaction statuses for a given List of transaction hashes.
+  try {
+    final result =
+        await client.transaction.getTransactionsStatuses([hashOne, hashTwo]);
+    print(result);
+  } on Exception catch (e) {
+    print('Exception when calling Transaction->GetTransactionsStatuses: $e\n');
+  }
+
+  try {
+    final result = await client.transaction.getTransactionEffectiveFee(hashOne);
+    print(result);
+  } on Exception catch (e) {
+    print(
+        'Exception when calling Transaction->GetTransactionEffectiveFee: $e\n');
+  }
+
+  try {
+    final result = await client.transaction
+        .getTransactionsCount([TransactionType.transfer]);
+    print(result);
+  } on Exception catch (e) {
+    print('Exception when calling Transaction->GetTransactionsCount: $e\n');
+  }
 }
